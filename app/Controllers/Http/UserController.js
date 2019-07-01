2e14859cb0a6fc0e57d6480e1664d23260c255d0
llucasmota <lucas.o.mota@gmail.com>
2019-06-30 23:01:41 -0300

    Adicionando código para criação de usuário

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class UserController {
   async store ({ request }) {
+    const data = request.only(['username', 'email', 'password'])
 
+    const user = await User.create(data)
+
+    return user
   }
 }
 
