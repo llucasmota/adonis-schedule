f7e1d94bbe4d97eaa6b5c86135d63b161310f1bc
llucasmota <lucas.o.mota@gmail.com>
2019-07-04 08:38:19 -0300

    Alterando rota update para que seja possível alterar a senha do usuário e username. Foi realizado tratamento para verificar validade da senha antiga informada, bem como se o cliente está informando o confirme_pass

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Hash = use('Hash')
 
 class UserController {
   async store ({ request }) {
@@ -10,17 +11,28 @@ class UserController {
 
     return user
   }
-  async update ({ request, response, auth }) {
-    try {
-      const username = request.only(['username'])
-      const user = await User.findByOrFail('username', auth.user.username)
-      user.username = username
-      user.merge(username)
-      await user.save()
+  // async update ({ request, response, auth }) {
+  //   try {
+  //     const username = request.only(['username'])
+  //     const user = await User.findByOrFail('username', auth.user.username)
+  //     user.username = username
+  //     user.merge(username)
+  //     await user.save()
 
-      return response.status(204).send(user)
-    } catch (err) {
-      return response.status(err.status).send({ error: { message: 'Algo não deu certo' } })
+  //     return response.status(204).send(user)
+  //   } catch (err) {
+  //     return response.status(err.status).send({ error: { message: 'Algo não deu certo' } })
+  //   }
+  // }
+  async update ({ request, response, auth: { user } }) {
+    const data = request.only(['username', 'password_new', 'confirm_pass', 'password_old'])
+    // verificando se a senha antiga informada é a mesma que está persistida
+    const isSame = await Hash.verify(data.password_old, user.password)
+    if (!isSame) {
+      return response.status(401).send({ message: { error: 'A senha antiga não é válida' } })
+    }
+    if (!data.confirm_pass) {
+      return response.status(400).send({ message: { error: 'A confirmação de senha não foi informada' } })
     }
   }
 }
